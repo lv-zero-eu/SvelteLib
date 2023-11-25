81671dd65f15fe3d3fd1db5cac0626a60c011d61
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-11-25 10:09:40 +0000

    Feat : Box

---
+++ src/lib/box/Box.svelte
@@ -0,0 +1,31 @@
+<div class="box">
+	<div class="header">
+		<slot name="header" />
+	</div>
+	<div>
+		<slot />
+	</div>
+	<div class="footer">
+		<slot name="footer" />
+	</div>
+</div>
+
+<style>
+	.box > *:has(> *) {
+		padding: 0.2rem 0.5rem 0.4rem 0.75rem;
+	}
+	.box {
+		border: 1px solid var(--border);
+		border-radius: var(--border-radius);
+		background-color: transparent;
+		width: fit-content;
+	}
+	.header:has(> *) {
+		border-bottom: 1px solid var(--border);
+		background-color: var(--bg-accent);
+	}
+	.footer:has(> *) {
+		border-top: 1px solid var(--border);
+		background-color: var(--bg-accent);
+	}
+</style>
